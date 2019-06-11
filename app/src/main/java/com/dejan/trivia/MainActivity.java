204760e595db9dff764c4942d89ff66b44335342
dejandeki <dejancaeu@gmail.com>
2019-06-11 17:36:13 +0200

    appController/singleton class

--- app/src/main/java/com/dejan/trivia/MainActivity.java
+++ app/src/main/java/com/dejan/trivia/MainActivity.java
@@ -3,6 +3,8 @@ package com.dejan.trivia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dejan.trivia.controller.AppController;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
--- app/src/main/java/com/dejan/trivia/controller/AppController.java
+++ app/src/main/java/com/dejan/trivia/controller/AppController.java
@@ -1,8 +1,11 @@
 package com.dejan.trivia.controller;
 
 import android.app.Application;
+import android.text.TextUtils;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class AppController extends Application {
 
@@ -10,5 +13,26 @@ public class AppController extends Application {
     private static AppController mInstance;
     private RequestQueue mRequestQueue;
 
-    
+    public static synchronized AppController getInstance(){
+        return mInstance;
+    }
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        mInstance = this;
+    }
+    public RequestQueue getRequestQueue(){
+        if(mRequestQueue == null){
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+    public <T> void addToRequestQueue(Request<T> req, String tag){
+        req.setTag(TextUtils.isEmpty(tag)? TAG : tag);
+        getRequestQueue().add(req);
+    }
+    public <T> void addToRequestQueue(Request <T> req){
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
 }
